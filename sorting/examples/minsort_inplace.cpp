6a9eeb84fe3681a4f26ae85938bd0a7009c2931d
Ibrahim Fakhir <ibrahim.fakhir@outlook.de>
2022-12-25 00:05:16 +0100

    added print_array func to examples

--- sorting/examples/minsort_inplace.cpp
+++ sorting/examples/minsort_inplace.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <iomanip>
+
+void print_array(const std::vector<int>& array) {
+  for(const int& num: array) {
+    std::cout << std::setw(5) << num;
+  }
+  std::cout << std::endl;
+}
 
 int main() {
 
