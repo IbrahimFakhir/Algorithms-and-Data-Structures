083227e6040f7ccb8c91b0b71e73d130bf919a91
Ibrahim Fakhir <ibrahim.fakhir@outlook.de>
2022-12-24 19:52:42 +0100

    added print_int_array function

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <vector>
+
+void print_int_array(const std::vector<int>& array) {
+
+    for (const int& num : array) {
+        std::cout << num << std::endl;
+    }
+
+}
 
 int main () {
 
-    
+    // test print_int_array function
+    // std::vector<int> array = {5,3,6};
+    // print_int_array(array);
 
 }
