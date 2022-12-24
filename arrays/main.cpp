caca0c5506d3f5e472ad1825e5bc977c06484885
Ibrahim Fakhir <ibrahim.fakhir@outlook.de>
2022-12-24 20:15:56 +0100

    added absolute_value_array function

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -4,7 +4,19 @@
 void print_int_array(const std::vector<int>& array) {
 
     for (const int& num : array) {
-        std::cout << num << std::endl;
+        std::cout << num;
+        std::cout << "\t";
+    }
+    std::cout << std::endl;
+
+}
+
+void absolute_value_array(std::vector<int>& array) {
+
+    for (int& num : array) {
+        if (num < 0) {
+            num = -1 * num;
+        }
     }
 
 }
@@ -13,6 +25,12 @@ int main () {
 
     // test print_int_array function
     // std::vector<int> array = {5,3,6};
+    // print_int_array(array);
+
+    // test absolute_value_array
+    // std::vector<int> array = {-4, 5, 7, -2, 0};
+    // print_int_array(array);
+    // absolute_value_array(array);
     // print_int_array(array);
 
 }
