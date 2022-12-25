c657247ff55c3b4a72017ba196fbf5980e085732
Ibrahim Fakhir <ibrahim.fakhir@outlook.de>
2022-12-26 00:58:08 +0100

    added merge function

--- sorting/examples/mergesort_outofplace.cpp
+++ sorting/examples/mergesort_outofplace.cpp
@@ -2,8 +2,21 @@
 
 #include <iostream>
 
+void print_int_array(const std::vector<int>& array) {
+
+    for (const int& num : array) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+}
+
 int main() {
 
-    
+    std::vector<int> a = {1, 3, 6, 6};
+    std::vector<int> b = {2, 4, 4, 7, 9, 10};
+    std::vector<int> c = merge(a, b);
+
+    print_int_array(c);
 
 }
--- sorting/src/mergesort_outofplace.cpp
+++ sorting/src/mergesort_outofplace.cpp
@@ -1,15 +1,37 @@
-/* */
-
 #include <mergesort_outofplace.hpp>
 
-std::vector<int> mergesort_outofplace(const std::vector<int>& array) {
+std::vector<int>mergesort_outofplace(const std::vector<int>& array) {
 
     
 
 }
 
-std::vector<int> merge(const std::vector<int>& links, const std::vector<int>& rechts) {
+std::vector<int> merge(const std::vector<int>& left, const std::vector<int>& right) {
 
-    
+    std::vector<int> merged_array(left.size() + right.size());
+
+    int i = 0;
+    int j = 0;
+
+    while (i < left.size() && j < right.size()) {
+        if (left[i] <= right[j]) {
+            merged_array[i+j] = left[i];
+            i = i + 1;
+        }
+        else {
+            merged_array[i+j] = right[j];
+            j = j + 1;
+        }
+    }
+    while (i < left.size()) {
+        merged_array[i+j] = left[i];
+        i = i + 1;
+    }
+    while (j < right.size()) {
+        merged_array[i+j] = right[j];
+        j = j + 1;
+    }
+
+    return merged_array;
 
 }
