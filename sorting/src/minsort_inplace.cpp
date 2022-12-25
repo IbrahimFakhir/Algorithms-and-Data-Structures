595ee022a7dd7f2a25da0372cf2ccbda58b9f2c7
Ibrahim Fakhir <ibrahim.fakhir@outlook.de>
2022-12-25 02:25:10 +0100

    defined minsort_inplace function

--- sorting/src/minsort_inplace.cpp
+++ sorting/src/minsort_inplace.cpp
@@ -1,7 +1,20 @@
 #include <minsort_inplace.hpp>
 
 void minsort_inplace(std::vector<int>& array) {
-    
-    
+
+    int swap_index;
+    int tmp;
+
+    for (int i = 0; i < array.size(); i++) {
+        swap_index = i;
+        for (int j = i; j < array.size(); j++) {      
+            if (array[j] < array[swap_index]) {
+                swap_index = j;
+            }
+        }
+        tmp = array[i];
+        array[i] = array[swap_index];
+        array[swap_index] = tmp;
+    }
 
 }
