136db10b4f7c35408a508cba01b6301707e5adbb
Ibrahim Fakhir <ibrahim.fakhir@outlook.de>
2022-12-26 14:59:41 +0100

    updated example of mergesort

--- sorting/examples/mergesort_outofplace.cpp
+++ sorting/examples/mergesort_outofplace.cpp
@@ -13,8 +13,12 @@ void print_int_array(const std::vector<int>& array) {
 
 int main() {
 
-    std::vector<int> a = {-3, 9, 32, 7};
-    std::vector<int> b = mergesort_outofplace(a);
-    print_int_array(b);
+    std::vector<int> a = {17, -3, 9, 32, 7};
+    std::cout << "unsorted array: " << std::endl;
+    print_int_array(a);
+
+    a = mergesort_outofplace(a);
+    std::cout << "sorted array: " << std::endl;
+    print_int_array(a);
 
 }
