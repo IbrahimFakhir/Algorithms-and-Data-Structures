b77d2a36bef4333bb4d42223507134936281b203
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2022-12-27 19:23:14 +0100

    implemented ListNode class

--- datatypes/src/linked_list.cpp
+++ datatypes/src/linked_list.cpp
@@ -1,1 +1,30 @@
 #include <linked_list.hpp>
+
+ListNode::ListNode(int new_data) {
+
+    data = new_data;
+    next = nullptr;
+    
+}
+
+int ListNode::get_data() {
+
+    return data;
+
+}
+void ListNode::set_data(int new_data) {
+
+    data = new_data;
+    
+}
+
+ListNodeptr ListNode::get_next() {
+
+    return next;
+
+}
+void ListNode::set_next(const std::shared_ptr<ListNode>& new_next) {
+
+    next = new_next;
+
+}
