7aeaf8f015f2d1f251ebe86d34a63937e0fb0aef
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2022-12-27 23:52:37 +0100

    added more thorough testing to linked_list

--- datatypes/linked_list/examples/linked_list.cpp
+++ datatypes/linked_list/examples/linked_list.cpp
@@ -7,6 +7,12 @@ int main() {
     List l = List();
 
     l.insert_front(5);
+    l.insert_front(3);
+    l.insert_after(l.next(l.get_head()), 6);
+    l.insert_front(-214);
+
+    l.remove_front();
+    l.remove_after(l.get_head());
 
     l.print_list();
 
