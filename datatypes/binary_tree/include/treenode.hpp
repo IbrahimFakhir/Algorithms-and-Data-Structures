f0505dcb128956b2a30ec6868ddd50acf1389d7f
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2022-12-28 01:48:34 +0100

    implemented TreeNode header

--- datatypes/binary_tree/include/treenode.hpp
+++ datatypes/binary_tree/include/treenode.hpp
@@ -1,3 +1,30 @@
 #pragma once
 
 #include <memory>
+
+class TreeNode {
+
+    public:
+        TreeNode(int data);
+
+        void set_data(int new_data);
+        int get_data();
+
+        std::shared_ptr<TreeNode> get_left_child();
+        std::shared_ptr<TreeNode> get_right_child();
+
+        void set_left_child();
+        void set_right_child();
+
+        void remove_left_child();
+        void remove_right_child();
+
+    private:
+        std::shared_ptr<TreeNode> left_child;
+        std::shared_ptr<TreeNode> right_child;
+
+        int data;
+
+};
+
+typedef std::shared_ptr<TreeNode> TreeNodeptr;
