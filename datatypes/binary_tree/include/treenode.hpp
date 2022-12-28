caeed18b069f870e215b43678d610505c6c8902f
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2022-12-28 02:36:47 +0100

    defined TreeNode class in src

--- datatypes/binary_tree/include/treenode.hpp
+++ datatypes/binary_tree/include/treenode.hpp
@@ -13,8 +13,8 @@ class TreeNode {
         std::shared_ptr<TreeNode> get_left_child();
         std::shared_ptr<TreeNode> get_right_child();
 
-        void set_left_child();
-        void set_right_child();
+        void set_left_child(int new_data);
+        void set_right_child(int new_data);
 
         void remove_left_child();
         void remove_right_child();
--- datatypes/binary_tree/src/binary_tree.cpp
+++ datatypes/binary_tree/src/binary_tree.cpp
@@ -1,4 +1,65 @@
 #include <binary_tree.hpp>
 #include <treenode.hpp>
 
+TreeNode::TreeNode(int new_data) {
 
+    data = new_data;
+
+    left_child = nullptr;
+    right_child = nullptr;
+
+}
+
+void TreeNode::set_data(int new_data) {
+
+    data = new_data;
+
+}
+int TreeNode::get_data() {
+
+    return data;
+
+}
+
+TreeNodeptr TreeNode::get_left_child() {
+
+    return left_child;
+
+}
+TreeNodeptr TreeNode::get_right_child() {
+
+    return right_child;
+
+}
+
+void TreeNode::set_left_child(int new_data) {
+
+    if (left_child) {
+        left_child->set_data(new_data);
+    }
+    else {
+        left_child = std::make_shared<TreeNode>(new_data);
+    }
+
+}
+void TreeNode::set_right_child(int new_data) {
+
+    if (right_child) {
+        right_child->set_data(new_data);
+    }
+    else {
+        // right_child = std::make_shared<TreeNodeptr>(new_data);
+    }
+
+}
+
+void TreeNode::remove_left_child() {
+
+    left_child = nullptr;
+
+}
+void TreeNode::remove_right_child() {
+
+    right_child = nullptr;
+
+}
