60c6b8f705ee29ea6fcc9c3046d88faf92981346
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2022-12-29 00:43:01 +0100

    implemented DoublyLinkedListnode class

--- datatypes/doubly_linked_list/src/doubly_linked_list.cpp
+++ datatypes/doubly_linked_list/src/doubly_linked_list.cpp
@@ -1,2 +1,44 @@
 #include <doubly_linked_list.hpp>
 #include <doubly_linked_listnode.hpp>
+
+DoublyLinkedListNode::DoublyLinkedListNode(int new_data) {
+
+    data = new_data;
+
+    next = nullptr;
+    // initialize prev??
+
+}
+
+DLinkedListNodeptr DoublyLinkedListNode::get_next() {
+    
+    return next;
+
+}
+void DoublyLinkedListNode::set_next(const std::shared_ptr<DoublyLinkedListNode>& new_next) {
+
+    next = new_next;
+
+}
+
+WeakDLinkedListNodeptr DoublyLinkedListNode::get_prev() {
+
+    return prev;
+
+}
+void DoublyLinkedListNode::set_prev(const std::weak_ptr<DoublyLinkedListNode>& new_prev) {
+
+    prev = new_prev;
+
+}
+
+int DoublyLinkedListNode::get_data() {
+
+    return data;
+
+}
+void DoublyLinkedListNode::set_data(int new_data) {
+
+    data = new_data;
+
+}
