3ae8d7831e2a3f48575d3dfab1de9ec740a4ad9d
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-01-02 03:34:46 +0100

    created hellalot Student objects

--- student/examples/student.cpp
+++ student/examples/student.cpp
@@ -2,18 +2,24 @@
 
 #include <iostream>
 
-void print_string_array(const std::vector<std::string>& array) {
-
-    for (const std::string& str : array) {
-        std::cout << str << std::endl;
-    }
-
-}
-
 int main() {
 
-    Student s = Student("a", "b", 2, 3, 4, 2.0, {"gg", "asd"});
-
-    s.print_student();
+    Student s1("Eren", "Akdag", 27, 7, 2002, 178);
+    Student s2("Margaret ", "Akdag", 24, 4, 1959, 162);
+    Student s3("Amy", "Roberts", 4, 6, 1954, 171);
+    Student s4("Lucas", "Dahmer", 28, 9, 2001, 165);
+    Student s5("Anna", "Todd", 21, 9, 1989, 155);
+    Student s6("Jeromy", "Lewis", 15, 4, 1952, 165);
+    Student s7("Mark", "Anderson", 13, 9, 1964, 183);
+    Student s8("Karen", "White", 1, 7, 2001, 166);
+    Student s9("Jamal", "Maron", 29, 3, 1999, 196);
+    Student s10("Kevin", "Gaton", 19, 11, 1967, 182);
+    Student s11("Zion", "Builder", 14, 5, 1989, 175);
+    Student s12("Kathrin", "Blank", 5, 12, 2004, 177);
+    Student s13("Jaquavis", "Light", 21, 10, 2002, 203);
+    Student s14("Bril", "Long", 15, 7, 2003, 154);
+    Student s15("Giannes", "Kump", 3, 2, 1985, 191);
+    Student s16("Marcel", "Pois", 6, 5, 1992, 189);
+    std::vector<Student> Students = {s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16};
 
 }
