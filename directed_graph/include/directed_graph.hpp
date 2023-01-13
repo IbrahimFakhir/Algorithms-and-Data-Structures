2e76dbf32ba4ee0c94a48b187912983b0bfeb138
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-01-13 16:50:46 +0100

    implemented first DirectedGraph functions

--- directed_graph/include/directed_graph.hpp
+++ directed_graph/include/directed_graph.hpp
@@ -12,7 +12,7 @@
 class NodeHash {
 
     public:
-        size_t operator() (const Node& k) const;
+        size_t operator() (const Node& n) const;
 
 };
 typedef std::unordered_set<Node, NodeHash> NodeSet;
@@ -20,7 +20,7 @@ typedef std::unordered_set<Node, NodeHash> NodeSet;
 class EdgeHash {
 
     public:
-        size_t operator() (const Edge& k) const;
+        size_t operator() (const Edge& e) const;
 
 };
 typedef std::unordered_set<Edge, EdgeHash> EdgeSet;
@@ -30,6 +30,30 @@ class DirectedGraph {
     public:
         DirectedGraph();
 
-        
+        void insert_node(const Node& n);
+        void insert_node(const index_t& idx);
+
+        void delete_node(const Node& n);
+        void delete_node(const index_t& idx);
+
+        bool exists_node(const Node& n) const;
+        bool exists_node(const index_t& idc) const;
+
+        const NodeSet& get_node_set() const;
+
+        // add existing edge
+        void insert_edge(const Edge& e);
+        // create and add edge
+        void insert_edge(const index_t& start, const index_t& end);
+
+        void delete_edge(const Edge& e);
+        void delete_edge(const index_t& start, const index_t& end);
+
+        bool exists_edge(const Edge& e) const;
+        bool exists_edge(const index_t& start, const index_t& end) const;
+
+    private:
+        NodeSet node_set;
+        EdgeSet edge_set;
 
 };
--- directed_graph/src/directed_graph.cpp
+++ directed_graph/src/directed_graph.cpp
@@ -1,13 +1,26 @@
 #include <directed_graph.hpp>
 
-size_t NodeHash::operator() (const Node& k) const {
+size_t NodeHash::operator() (const Node& n) const {
     
-    return k.get_index_t();
+    return n.get_index_t();
 
 }
 
-size_t EdgeHash::operator() (const Edge& k) const {
+size_t EdgeHash::operator() (const Edge& e) const {
 
-    return k.get_start_node().get_index_t() ^ k.get_end_node().get_index_t();
+    return e.get_start_node().get_index_t() ^ e.get_end_node().get_index_t();
+
+}
+
+DirectedGraph::DirectedGraph() {}
+
+void DirectedGraph::insert_node(const Node& n) {
+
+    node_set.emplace(n);
+
+}
+void DirectedGraph::insert_node(const index_t& idx) {
+
+    insert_node(Node(idx));
 
 }
