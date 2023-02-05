f77da1c4cdb5c19a047a71239091abe1fa5c6901
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-02-05 19:37:52 +0100

    added examples

--- sorting/examples/minsort_outofplace.cpp
+++ sorting/examples/minsort_outofplace.cpp
@@ -4,6 +4,16 @@
 
 int main() {
 
-    
+    std::vector<int> unsorted = {5, 2, 7, 8, 1};
+    for (int& num : unsorted) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    std::vector<int> sorted = minsort_outofplace(unsorted);
+    for (int& num : sorted) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
 
 }
--- sorting/src/minsort_outofplace.cpp
+++ sorting/src/minsort_outofplace.cpp
@@ -1,1 +1,7 @@
 #include <minsort_outofplace.hpp>
+
+std::vector<int> minsort_outofplace(std::vector<int> input_array) {
+
+    return {5, 6};
+
+}
