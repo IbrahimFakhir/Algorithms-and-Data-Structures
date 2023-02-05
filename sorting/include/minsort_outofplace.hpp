801b80149ea474b56183ef52f91b8c7344551dfd
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-02-05 19:32:32 +0100

    added documentation

--- sorting/include/minsort_outofplace.hpp
+++ sorting/include/minsort_outofplace.hpp
@@ -2,4 +2,11 @@
 
 #include <vector>
 
+/*
+*
+* returns a sorted copy of the input array
+*
+* @param given_array input array to be sorted
+* 
+*/
 std::vector<int> minsort_outofplace(std::vector<int> given_array);
