a69865cbfe7c9291731419a38c6aef680cb1afb1
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-02-06 18:08:21 +0100

    updated mergesort_outofplace

--- sorting/examples/mergesort_outofplace.cpp
+++ sorting/examples/mergesort_outofplace.cpp
@@ -1,24 +1,21 @@
 #include <mergesort_outofplace.hpp>
 
 #include <iostream>
+#include <iomanip>
 
-void print_int_array(const std::vector<int>& array) {
-
-    for (const int& num : array) {
-        std::cout << num << " ";
-    }
-    std::cout << std::endl;
-
+void print_array(const std::vector<int>& array) {
+  for(const int& num: array) {
+    std::cout << std::setw(5) << num;
+  }
+  std::cout << std::endl;
 }
 
 int main() {
 
-    std::vector<int> a = {17, -3, 9, 32, 7};
-    std::cout << "unsorted array: " << std::endl;
-    print_int_array(a);
+    std::vector<int> a = {17, -3, 9, 32, 32, 7, 6};
+    print_array(a);
 
     a = mergesort_outofplace(a);
-    std::cout << "sorted array: " << std::endl;
-    print_int_array(a);
+    print_array(a);
 
 }
--- sorting/src/mergesort_outofplace.cpp
+++ sorting/src/mergesort_outofplace.cpp
@@ -1,15 +1,17 @@
 #include <mergesort_outofplace.hpp>
 
-std::vector<int>mergesort_outofplace(const std::vector<int>& array) {
+std::vector<int> mergesort_outofplace(const std::vector<int>& array) {
 
     if (array.size() == 1) {
         return array;
     }
 
     int mid = ceil(array.size() / 2.0);
+    std::vector<int> left (&array[0], &array[mid]);
+    std::vector<int> right (&array[mid], &array[array.size()]);
 
-    std::vector<int> left = mergesort_outofplace(std::vector<int>{array.begin(), array.end() - mid});
-    std::vector<int> right = mergesort_outofplace(std::vector<int>{array.begin() + mid - (array.size() % 2), array.end()});
+    left = mergesort_outofplace(left);
+    right = mergesort_outofplace(right);
 
     return merge(left, right);
 
@@ -17,30 +19,29 @@ std::vector<int>mergesort_outofplace(const std::vector<int>& array) {
 
 std::vector<int> merge(const std::vector<int>& left, const std::vector<int>& right) {
 
-    std::vector<int> merged_array(left.size() + right.size());
+    std::vector<int> merged(left.size() + right.size());
 
-    int i = 0;
-    int j = 0;
+    int i,j = 0;
 
-    while (i < left.size() && j < right.size()) {
-        if (left[i] <= right[j]) {
-            merged_array[i+j] = left[i];
-            i = i + 1;
+    while (i < left.size() && i < right.size()) {
+        if (left[i] < right[j]) {
+            merged[i+j] = left[i];
+            i++;
         }
         else {
-            merged_array[i+j] = right[j];
-            j = j + 1;
+            merged[i+j] = right[j];
+            j++;
         }
     }
     while (i < left.size()) {
-        merged_array[i+j] = left[i];
-        i = i + 1;
+        merged[i+j] = left[i];
+        i++;
     }
     while (j < right.size()) {
-        merged_array[i+j] = right[j];
-        j = j + 1;
+        merged[i+j] = right[j];
+        j++;
     }
 
-    return merged_array;
+    return merged;
 
 }
